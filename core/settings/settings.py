82f00b285a546d55a41ebfc8c8aee56b973df479
Noman Majeed <nomanmajeed986@gmail.com>
2021-08-14 02:36:22 +0500

    django settings.py updated

--- core/settings/settings.py
+++ core/settings/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 STATIC_URL = "/static/"
 
